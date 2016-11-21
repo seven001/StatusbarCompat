4673cf2b5f5003cb6c21c07c1b47954cad5a8ee8
seven001 <731739299@qq.com>
2016-11-21 15:11:11 +0800

    修改bug提交

--- app/src/main/java/com/seven/statusbarcompat/utils/StatusBarCompat.java
+++ app/src/main/java/com/seven/statusbarcompat/utils/StatusBarCompat.java
@@ -19,7 +19,7 @@ public class StatusBarCompat
     private static final int INVALID_VAL = -1;
     private static final int COLOR_DEFAULT = Color.parseColor("#20000000");
 
-    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static void compat(Activity activity, int statusColor)
     {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
